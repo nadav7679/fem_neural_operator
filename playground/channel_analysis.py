a00b61f176c6b90cd0bfa65fd12549374d8bfbbb
nadav7679 <nadav76799@gmail.com>
2024-06-10 15:32:17 +0100

    Added Firedrake loss in the analysis

--- playground/channel_analysis.py
+++ playground/channel_analysis.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import firedrake as fd
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
@@ -9,7 +10,7 @@ from network import NeuralNetworkTrainer, NonlocalNeuralOperator
 from burgers import BurgersDataset, fourier_coefficients
 
 
-def average_loss(models: List[NonlocalNeuralOperator], datasets: BurgersDataset, h: float):
+def average_loss(models: List[NonlocalNeuralOperator], dataset: BurgersDataset, h: float):
     target = dataset[:][1]
 
     losses = torch.zeros(len(models), dtype=torch.float32)
@@ -24,11 +25,35 @@ def average_loss(models: List[NonlocalNeuralOperator], datasets: BurgersDataset,
     return losses, parameters
 
 
+def average_firedrake_loss(models, dataset, nx):
+    targets = dataset[:][1].squeeze(1).detach().cpu().numpy()
+
+    filename = f"data/burgers__samples_1000__nx_{nx}"
+    with fd.CheckpointFile(f"{filename}__mesh.h5", "r") as file:
+        function_space = fd.FunctionSpace(file.load_mesh(), "CG", 1)
+
+    losses = []
+    for model in models:
+        predictions = model(dataset[:][0]).squeeze(1).detach().cpu().numpy()
+
+        loss = 0
+        for target, predict in zip(targets, predictions):
+            loss += fd.errornorm(
+                fd.Function(function_space, val=target),
+                fd.Function(function_space, val=predict),
+                "L1"
+            )
+
+        losses.append(loss / len(targets))
+
+    return losses
+
+
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     nx = 256
-    h = 1/nx
+    h = 1 / nx
     max_modes = 10
     d_arr = torch.arange(5, 105, 5).to(dtype=torch.int)
 
@@ -39,12 +64,13 @@ if __name__ == "__main__":
 
     models = [torch.load(f"models/nx_{nx}__d_{d}__max_modes_{max_modes}.pt") for d in d_arr]
     losses, parameters = average_loss(models, dataset, h)
+    losses_fd = average_firedrake_loss(models, dataset, nx)
 
-    for d, loss, param in zip(d_arr, losses, parameters):
-        print(rf"${d}$ & ${loss:.05}$ & {param}\\")
-        # print(f"d: {d:03} | Parameters: {param:06} | Average loss: {loss:.04}")
+    for d, loss, loss_fd, param in zip(d_arr, losses, losses_fd, parameters):
+        # print(rf"${d}$ & ${loss:.05}$ & {param}\\")
+        print(f"d: {d:03} | Parameters: {param:06} | Average loss: {loss:.04} | Firedrake loss: {loss_fd:.04}")
 
-    plt.plot(d_arr, losses)
+    plt.plot(d_arr, losses_fd)
     plt.title(f"L1 average loss for Burgers with varying d nx={nx}")
     plt.xlabel("d - channels")
     # plt.yscale("log")
--- playground/mesh_analysis.py
+++ playground/mesh_analysis.py
@@ -1,20 +1,48 @@
 from typing import List
 
+import firedrake as fd
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 from network import NeuralNetworkTrainer, NonlocalNeuralOperator
 from burgers import BurgersDataset, fourier_coefficients
 
 
-def average_loss(models: List[NonlocalNeuralOperator], datasets: List[BurgersDataset], h_arr: List[float]):
+def average_coefficient_loss(models: List[NonlocalNeuralOperator], datasets: List[BurgersDataset], h_arr: List[float]):
     losses = []
     for model, dataset, h in zip(models, datasets, h_arr):
         target = dataset[:][1]
         prediction = model(dataset[:][0])
         losses.append(h / len(target) * nn.L1Loss(reduction='sum')(target, prediction))
+
+    return losses
+
+
+def average_firedrake_loss(models, datasets, nx_arr, ):
+    losses = []
+    for model, dataset, nx, in zip(models, datasets, nx_arr):
+        filename = f"data/burgers__samples_1000__nx_{nx}"
+        with fd.CheckpointFile(f"{filename}__mesh.h5", "r") as file:
+            function_space = fd.FunctionSpace(file.load_mesh(), "CG", 1)
+
+
+        targets = dataset[:][1].squeeze(1).detach().cpu().numpy()
+        predictions = model(dataset[:][0]).squeeze(1).detach().cpu().numpy()
+        # samples = dataset[:][0][0].detach().cpu().numpy()
+
+        loss = 0
+        for target, predict in zip(targets, predictions):
+            loss += fd.errornorm(
+                fd.Function(function_space, val=target),
+                fd.Function(function_space, val=predict),
+                "L1"
+            )
+
+        losses.append(loss / len(targets) )
 
     return losses
 
@@ -39,7 +67,8 @@ if __name__ == "__main__":
         models.append(torch.load(f"models/nx_{nx}__d_{d}__max_modes_{max_modes}.pt"))
         datasets.append(dataset[int(0.8 * len(dataset)):])  # Cutting off the train data
 
-    losses = average_loss(models, datasets, h_arr)
+    losses_coeff = average_coefficient_loss(models, datasets, h_arr)
+    losses_fd = average_firedrake_loss(models, datasets, nx_arr)
 
-    for model, nx, loss in zip(models, nx_arr, losses):
-        print(f"nx: {nx:03} | Average loss: {loss:.04}")
+    for model, nx, loss_fd, loss_coeff in zip(models, nx_arr, losses_fd, losses_coeff):
+        print(f"nx: {nx:03} | Average Firedrake loss: {loss_fd:.04} | Average coeff loss: {loss_coeff:.04}")
