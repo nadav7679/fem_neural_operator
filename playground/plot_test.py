f875b25978f308da3b472ae7be781740dbe04e99
nadav7679 <nadav76799@gmail.com>
2024-06-10 12:53:15 +0100

    Updated plot_test.py

--- playground/plot_test.py
+++ playground/plot_test.py
@@ -3,31 +3,36 @@ import firedrake as fd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from burgers import BurgersDataset
+
+
 if __name__ == "__main__":
+    nx = 100
+    max_modes = 10
+    d = 10
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    filename = "data/burgers__samples_1000__nx_100"
+    filename = f"data/burgers__samples_1000__nx_{nx}"
 
-    net = torch.load("model.pt").to(device=device)
-    data = torch.load(f"{filename}.pt").to(device=device, dtype=torch.float32)
-
+    net = torch.load(f"models/nx_{nx}__d_{d}__max_modes_{max_modes}.pt").to(device=device)
     param_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
     print(f"Number of parameters: {param_num}")
 
+    data = torch.load(f"{filename}.pt").unsqueeze(2).to(device=device, dtype=torch.float32)
+    grid = torch.linspace(0, 1, nx, device=device)
+    testset = BurgersDataset(data[int(0.8 * len(data)):], grid)
+
+    prediction_coeffs = net(testset[:][0]).detach().cpu()
+
     with fd.CheckpointFile(f"{filename}__mesh.h5", "r") as file:
-        mesh = file.load_mesh()
-
-    function_space = fd.FunctionSpace(mesh, "CG", degree=1)
-
-    simulation_coeff = net(data[:, 0, None, :]).cpu().detach().numpy()
-    data = data.to(device="cpu").numpy()
+        function_space = fd.FunctionSpace(file.load_mesh(), "CG", degree=1)
 
     initial_functions = []
     solutions = []
-    simulations = []
-    for i, arr in enumerate(data):
-        initial_functions.append(fd.Function(function_space, val=arr[0]))
-        solutions.append(fd.Function(function_space, val=arr[1]))
-        simulations.append(fd.Function(function_space, val=simulation_coeff[i, 0, :]))
+    predictions = []
+    for predict, datapoint in zip(prediction_coeffs, testset):
+        initial_functions.append(fd.Function(function_space, val=datapoint[0][0].detach().cpu()))
+        solutions.append(fd.Function(function_space, val=datapoint[1][0].detach().cpu()))
+        predictions.append(fd.Function(function_space, val=predict[0]))
 
     # Number of points for plotting
     num_plot_points = 1000
@@ -37,8 +42,8 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
 
     # Plot each function
-    index = 10
-    for func, label in zip([solutions[index], simulations[index], initial_functions[index]], ["Ground truth", "Result", "initial"]):
+    index = 40
+    for func, label in zip([solutions[index], predictions[index], initial_functions[index]], ["Firedrake solution", "Network solution", "Initial condition"]):
         # Evaluate the function at the plot points
         plot_values = np.array([func.at(x) for x in plot_points])
 
@@ -46,9 +51,8 @@ if __name__ == "__main__":
         ax.plot(plot_points, plot_values, label=label)
 
     # Set plot labels and title
-    ax.set_xlabel('x')
-    ax.set_ylabel('Value')
-    ax.set_title('Multiple Firedrake Functions')
+    ax.set_xlabel("$x$")
+    ax.set_title(f"Burgers Firedrake and Net solutions, $N_x={nx}$")
     ax.legend()
 
     # Show the plot
