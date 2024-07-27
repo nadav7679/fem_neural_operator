b849e1260d70f70d9d721f2e20259360a493b524
nadav7679 <nadav76799@gmail.com>
2024-07-27 15:22:10 +0100

    KS downsampling of varying T datasets

--- playground/data/KS/samples/downsample.py
+++ playground/data/KS/samples/downsample.py
@@ -2,7 +2,7 @@ from firedrake import *
 import torch
 
 
-def downsample(data, mesh1, mesh2, N):
+def downsample(data, mesh1, mesh2, N, T):
     her1 = FunctionSpace(mesh1, "HER", 3)
     her2 = FunctionSpace(mesh2, "HER", 3) 
     cg1 = FunctionSpace(mesh1, "CG", 1)
@@ -23,19 +23,19 @@ def downsample(data, mesh1, mesh2, N):
             downsampled_data[i, j, :] = func_her2.dat.data[:]
             
     
-    torch.save(torch.Tensor(downsampled_data), f"N{N}_HER_nu0029_T01_samples1200.pt")
+    torch.save(torch.Tensor(downsampled_data), f"N{N}_HER_nu0029_T{T}_samples1200.pt")
 
 
 if __name__ == "__main__":
-    data = torch.load("/home/clustor2/ma/n/np923/fem_neural_operator/playground/data/KS/samples/N4096_HER_nu0029_T01_samples1200.pt").numpy()
     with CheckpointFile(f"/home/clustor2/ma/n/np923/fem_neural_operator/playground/data/KS/meshes/N4096.h5", "r") as f:
         mesh1 = f.load_mesh()
 
-    
-    for N in [64, 128, 256, 512, 1024, 2048]:
-        with CheckpointFile(f"/home/clustor2/ma/n/np923/fem_neural_operator/playground/data/KS/meshes/N{N}.h5", "r") as f:
-            mesh2 = f.load_mesh()
-
-        downsample(data, mesh1, mesh2, N)
+    for T in ["02", "03", "04", "05"]:
+        data = torch.load(f"/home/clustor2/ma/n/np923/fem_neural_operator/playground/data/KS/samples/N4096_HER_nu0029_T{T}_samples1200.pt").numpy()
+        for N in [64, 128, 256, 512, 1024, 2048]:
+            with CheckpointFile(f"/home/clustor2/ma/n/np923/fem_neural_operator/playground/data/KS/meshes/N{N}.h5", "r") as f:
+                mesh2 = f.load_mesh()
+            print(f"Working on T={T}, N={N}")
+            downsample(data, mesh1, mesh2, N, T)
 
 
