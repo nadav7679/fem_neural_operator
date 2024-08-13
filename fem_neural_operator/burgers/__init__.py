dea79891fc2df38c34877d228e6c5e4fa8cd9df8
nadav7679 <nadav76799@gmail.com>
2024-08-13 16:14:31 +0100

    File reorder

--- fem_neural_operator/burgers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .BurgersDataset import BurgersDataset
-from .BurgersSolver import BurgersSolver
-from .utils import fourier_coefficients
--- fem_neural_operator/data_generation/burgers_generator.py
+++ fem_neural_operator/data_generation/burgers_generator.py
@@ -4,8 +4,62 @@ This file generates initial conditions and solutions to Burgers Equation using f
 import torch
 from firedrake import *
 import argparse
+import numpy as np
+from firedrake.pyplot import plot
+import matplotlib.pyplot as plt
 
-from BurgersSolver import BurgersSolver
+
+class BurgersSolver:
+    """
+    A class that solves burgers equation for a given initial condition and mesh.
+    """
+    def __init__(self, mesh: PeriodicIntervalMesh, nu: float, degree: int, t_end: float):
+        """
+
+        Args:
+            mesh: Periodic mesh
+            nu: The constant \nu from Burgers
+            degree: Degree of Lagrange function space
+            t_end: Final time for simulation
+        """
+        self.mesh = mesh
+        self.t_end = t_end
+        self.dt = 1./(10*len(mesh.cell_sizes.dat.data))  # Equispaced so nx=const
+        self.nu = Constant(nu)
+        self.x = SpatialCoordinate(self.mesh)[0]
+        self.space = FunctionSpace(self.mesh, "Lagrange", degree)
+
+        self.u_n1 = Function(self.space, name="u^{n+1}")
+        self.u_n = Function(self.space, name="u^{n}")
+        self.v = TestFunction(self.space)
+
+        f = (((self.u_n1 - self.u_n)) * self.v +
+             self.dt * self.u_n1 * self.u_n1.dx(0) * self.v +
+             self.dt * self.nu*self.u_n1.dx(0)*self.v.dx(0)) * dx
+
+        problem = NonlinearVariationalProblem(f, self.u_n1)
+        self.solver = NonlinearVariationalSolver(problem)
+
+    def solve(self, u_init, running_plot=False):
+        self.u_n.interpolate(u_init)
+
+        self.u_n1.assign(self.u_n)
+
+        for t in np.linspace(0, self.t_end, int(self.t_end/self.dt)):
+            if running_plot:
+                if t == 0:
+                    fig, axes = plt.subplots()
+
+                axes.set_title(f"t={t:.3f}")
+                axes.grid()
+                plot(self.u_n, axes=axes)
+                plt.pause(0.0001)
+                axes.clear()
+
+            self.solver.solve()
+            self.u_n.assign(self.u_n1)
+
+        return self.u_n
 
 
 def gaussian_field(cg_space, dg_space, cell_area, rg, d=25., s=625.):
@@ -33,6 +87,7 @@ def gaussian_field(cg_space, dg_space, cell_area, rg, d=25., s=625.):
     solve(a == F, uh_2)
 
     return uh_2
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Generate Burgers data")
@@ -74,6 +129,26 @@ if __name__ == "__main__":
         data[i, 1] = torch.tensor(sol.dat.data)
 
     torch.save(data, f"{args.project_dir}/playground/data/samples/N{args.N}_nu{args.nu}_samples{args.samples}_batch{args.batch}.pt")
+    
+    
+    # -------- Test BurgersSolver ----------#
+    # mesh = PeriodicIntervalMesh(100, 1)
+    # params = {
+    #     "mesh": mesh,
+    #     "nu": 1e-2,
+    #     "degree": 2,
+    #     "t_end": 1
+    # }
+    
+    # burgers = BurgersSolver(**params)
+    # u_init = sin(2*pi*burgers.x)
+    # u = burgers.solve(u_init, running_plot=True)
+        
+    # fig, axes = plt.subplots()
+    # plt.grid()
+    # plot(u, axes=axes)
+    # plt.savefig("media/fig")
+
 
 
 
