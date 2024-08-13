68b5354bd673031bf5885a4ba6fe7f896e3f8ca2
nadav7679 <nadav76799@gmail.com>
2024-08-13 16:17:36 +0100

    Added Dataset import to init

--- fem_neural_operator/classes/__init__.py
+++ fem_neural_operator/classes/__init__.py
@@ -2,3 +2,4 @@ from .NetworkTrainer import NeuralNetworkTrainer
 from .NeuralOperatorNetwork import NeuralOperatorNetwork, NeuralOperatorLayer
 from .NeuralOperatorModel import BurgersModel, KSModel
 from .ProjectionCoefficient import ProjectionCoefficient
+from .Dataset import Dataset
