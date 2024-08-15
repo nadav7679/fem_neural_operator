a5f4002e2965d85a039a2df4987b8d1a6af9df0d
nadav7679 <nadav76799@gmail.com>
2024-08-15 11:32:39 +0100

    KS data generation concatenated downsampled samples

--- fem_neural_operator/data/KS/samples/downsample.py
+++ fem_neural_operator/data/KS/samples/downsample.py
@@ -23,15 +23,15 @@ def downsample(data, mesh1, mesh2, N, T):
             downsampled_data[i, j, :] = func_her2.dat.data[:]
             
     
-    torch.save(torch.Tensor(downsampled_data), f"N{N}_HER_nu0029_T{T}_samples1200.pt")
+    torch.save(torch.Tensor(downsampled_data), f"N{N}_HER_nu0029_T{T[0] + T[-1]}_samples1200.pt")
 
 
 if __name__ == "__main__":
     with CheckpointFile(f"/home/clustor2/ma/n/np923/fem_neural_operator/fem_neural_operator/data/KS/meshes/N4096.h5", "r") as f:
         mesh1 = f.load_mesh()
 
-    for T in ["02", "03", "04", "05"]:
-        data = torch.load(f"/home/clustor2/ma/n/np923/fem_neural_operator/fem_neural_operator/data/KS/samples/N4096_HER_nu0029_T{T}_samples1200.pt").numpy()
+    for T in ["0.6", "0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5"]:
+        data = torch.load(f"/home/clustor2/ma/n/np923/fem_neural_operator/fem_neural_operator/data/KS/samples/N4096_HER_nu0029_T{T[0] + T[-1]}_samples1200.pt").numpy()
         for N in [64, 128, 256, 512, 1024, 2048]:
             with CheckpointFile(f"/home/clustor2/ma/n/np923/fem_neural_operator/fem_neural_operator/data/KS/meshes/N{N}.h5", "r") as f:
                 mesh2 = f.load_mesh()
